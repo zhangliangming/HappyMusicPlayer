8f72b6df72fae8429c46c7389e19decf7e4f2452
zhangliangming <316257874@qq.com>
2015-03-13 18:53:57 +0800

    添加启动页面时间提示

--- gen/com/happyplayer/ui/R.java
+++ gen/com/happyplayer/ui/R.java
@@ -8,6 +8,11 @@
 package com.happyplayer.ui;
 
 public final class R {
+    public static final class anim {
+        public static final int anim_in=0x7f040000;
+        public static final int anim_out=0x7f040001;
+        public static final int anim_stay=0x7f040002;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -16,13 +21,13 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040005;
-        public static final int activity_vertical_margin=0x7f040006;
-        public static final int bottom_player_height=0x7f040002;
-        public static final int bottom_tab_padding_left=0x7f040004;
-        public static final int main_tab_button_textsize=0x7f040003;
-        public static final int title_height=0x7f040000;
-        public static final int title_icon_image=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050005;
+        public static final int activity_vertical_margin=0x7f050006;
+        public static final int bottom_player_height=0x7f050002;
+        public static final int bottom_tab_padding_left=0x7f050004;
+        public static final int main_tab_button_textsize=0x7f050003;
+        public static final int title_height=0x7f050000;
+        public static final int title_icon_image=0x7f050001;
     }
     public static final class drawable {
         public static final int btn_download_default=0x7f020000;
@@ -44,43 +49,45 @@ public final class R {
         public static final int kg_btn_navigation_localmusic_play_pressed=0x7f020010;
         public static final int kg_ic_playing_bar_drag_closed=0x7f020011;
         public static final int kg_ic_playing_bar_drag_opened=0x7f020012;
-        public static final int main_tab_find=0x7f020013;
-        public static final int main_tab_my=0x7f020014;
-        public static final int main_tab_search=0x7f020015;
-        public static final int playing_bar_default_avatar=0x7f020016;
-        public static final int scan_to_be_started_icon_def_1=0x7f020017;
-        public static final int scan_to_be_started_icon_deff=0x7f020018;
-        public static final int splash=0x7f020019;
-        public static final int tab_discovery=0x7f02001a;
-        public static final int tab_discovery_s=0x7f02001b;
-        public static final int tab_download=0x7f02001c;
-        public static final int tab_download_s=0x7f02001d;
-        public static final int tab_personal=0x7f02001e;
-        public static final int tab_personal_s=0x7f02001f;
-        public static final int tab_search=0x7f020020;
-        public static final int tab_search_s=0x7f020021;
-        public static final int title_icon=0x7f020022;
+        public static final int kg_round_copy_bg=0x7f020013;
+        public static final int main_tab_find=0x7f020014;
+        public static final int main_tab_my=0x7f020015;
+        public static final int main_tab_search=0x7f020016;
+        public static final int playing_bar_default_avatar=0x7f020017;
+        public static final int scan_to_be_started_icon_def_1=0x7f020018;
+        public static final int scan_to_be_started_icon_deff=0x7f020019;
+        public static final int splash=0x7f02001a;
+        public static final int tab_discovery=0x7f02001b;
+        public static final int tab_discovery_s=0x7f02001c;
+        public static final int tab_download=0x7f02001d;
+        public static final int tab_download_s=0x7f02001e;
+        public static final int tab_personal=0x7f02001f;
+        public static final int tab_personal_s=0x7f020020;
+        public static final int tab_search=0x7f020021;
+        public static final int tab_search_s=0x7f020022;
+        public static final int title_icon=0x7f020023;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int bottom_wrapper=0x7f080009;
-        public static final int flag=0x7f08000a;
-        public static final int itempic=0x7f080011;
-        public static final int line=0x7f080002;
-        public static final int local=0x7f08000d;
-        public static final int navPlayImageButton=0x7f080010;
-        public static final int num=0x7f08000f;
-        public static final int player_bar_bg=0x7f080008;
-        public static final int singer_pic=0x7f08000b;
-        public static final int tab=0x7f080003;
-        public static final int tab_find=0x7f080005;
-        public static final int tab_my=0x7f080004;
-        public static final int tab_search=0x7f080006;
-        public static final int time=0x7f08000c;
-        public static final int title=0x7f08000e;
-        public static final int titleRelativeLayout=0x7f080000;
-        public static final int title_icon=0x7f080001;
-        public static final int viewpager=0x7f080007;
+        public static final int action_settings=0x7f090013;
+        public static final int bottom_wrapper=0x7f090009;
+        public static final int flag=0x7f09000a;
+        public static final int itempic=0x7f090012;
+        public static final int line=0x7f090002;
+        public static final int local=0x7f09000e;
+        public static final int navPlayImageButton=0x7f090011;
+        public static final int num=0x7f090010;
+        public static final int player_bar_bg=0x7f090008;
+        public static final int singer_pic=0x7f09000b;
+        public static final int splash=0x7f09000d;
+        public static final int tab=0x7f090003;
+        public static final int tab_find=0x7f090005;
+        public static final int tab_my=0x7f090004;
+        public static final int tab_search=0x7f090006;
+        public static final int time=0x7f09000c;
+        public static final int title=0x7f09000f;
+        public static final int titleRelativeLayout=0x7f090000;
+        public static final int title_icon=0x7f090001;
+        public static final int viewpager=0x7f090007;
     }
     public static final class layout {
         public static final int activity_guide=0x7f030000;
@@ -91,24 +98,24 @@ public final class R {
         public static final int fragment_search=0x7f030005;
     }
     public static final class menu {
-        public static final int find=0x7f070000;
-        public static final int my=0x7f070001;
-        public static final int search=0x7f070002;
+        public static final int find=0x7f080000;
+        public static final int my=0x7f080001;
+        public static final int search=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050007;
-        public static final int app_name=0x7f050000;
-        public static final int exit_tip=0x7f050001;
-        public static final int hello_world=0x7f050008;
-        public static final int tab_download=0x7f050004;
-        public static final int tab_find=0x7f050003;
+        public static final int action_settings=0x7f060007;
+        public static final int app_name=0x7f060000;
+        public static final int exit_tip=0x7f060001;
+        public static final int hello_world=0x7f060008;
+        public static final int tab_download=0x7f060004;
+        public static final int tab_find=0x7f060003;
         /**  主页面 
          */
-        public static final int tab_my=0x7f050002;
-        public static final int tab_search=0x7f050005;
-        public static final int title_activity_find=0x7f050009;
-        public static final int title_activity_my=0x7f050006;
-        public static final int title_activity_search=0x7f05000a;
+        public static final int tab_my=0x7f060002;
+        public static final int tab_search=0x7f060005;
+        public static final int title_activity_find=0x7f060009;
+        public static final int title_activity_my=0x7f060006;
+        public static final int title_activity_search=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -132,11 +139,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int main_tab_button=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int main_tab_button=0x7f070002;
     }
 }
--- src/com/happyplayer/ui/SplashActivity.java
+++ src/com/happyplayer/ui/SplashActivity.java
@@ -6,11 +6,18 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.happyplayer.common.Constants;
 import com.happyplayer.util.ActivityManager;
 
 public class SplashActivity extends Activity {
+	/**
+	 * 默认时
+	 */
+	private final static int UPDATE = -1;
 	/**
 	 * 跳转到主页页面
 	 */
@@ -23,7 +30,11 @@ public class SplashActivity extends Activity {
 	/**
 	 * 设置页面跳转时的延迟时间
 	 */
-	private static final long SPLASH_DELAY_MILLIS = 3000;
+	private static final int SPLASH_DELAY_MILLIS = 5000;
+
+	private ImageView splashImageView = null;
+
+	private TextView timeTextView = null;
 
 	/**
 	 * 根据msg.what来跳转到不同的页面
@@ -38,6 +49,12 @@ public class SplashActivity extends Activity {
 				break;
 			case GO_GUIDE:
 				goGuide();
+				break;
+			case UPDATE:
+				int ctime = (Integer) msg.obj;
+				int stime = (SPLASH_DELAY_MILLIS - ctime) / 1000;
+				timeTextView.setVisibility(View.VISIBLE);
+				timeTextView.setText(stime + "");
 				break;
 			default:
 				break;
@@ -57,19 +74,54 @@ public class SplashActivity extends Activity {
 	 * 初始化
 	 */
 	private void init() {
-		/***
-		 * // 1.进入页面后，判断用户是不是第一次使用该应用，如果是则跳转到应用的导航页面 //
-		 */
-		SharedPreferences preferences = getSharedPreferences(
-				Constants.SHARE_PREFERENCE_NAME, 0);
-		// 获取是否是第一次使用的参数
-		boolean isTheFirst = preferences.getBoolean(Constants.THE_FIRST_KEY,
-				Constants.THE_FIRST);
-		if (!isTheFirst) {
-			handler.sendEmptyMessageDelayed(GO_GUIDE, SPLASH_DELAY_MILLIS);
-		} else {
-			handler.sendEmptyMessageDelayed(GO_HOME, SPLASH_DELAY_MILLIS);
+		splashImageView = (ImageView) findViewById(R.id.splash);
+		timeTextView = (TextView) findViewById(R.id.time);
+		timeTextView.setVisibility(View.INVISIBLE);
+
+		Thread thread = new Thread(new TimeTipRunnable());
+		thread.start();
+	}
+
+	class TimeTipRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			int time = 0;
+			Message msg = null;
+			while (time <= SPLASH_DELAY_MILLIS) {
+				try {
+					Thread.sleep(1000);
+					msg = new Message();
+					msg.what = UPDATE;
+					msg.obj = time;
+					handler.sendMessage(msg);
+					msg = null;
+					time = time + 1000;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			/***
+			 * // 1.进入页面后，判断用户是不是第一次使用该应用，如果是则跳转到应用的导航页面 //
+			 */
+			SharedPreferences preferences = getSharedPreferences(
+					Constants.SHARE_PREFERENCE_NAME, 0);
+			// 获取是否是第一次使用的参数
+			boolean isTheFirst = preferences.getBoolean(
+					Constants.THE_FIRST_KEY, Constants.THE_FIRST);
+			if (!isTheFirst) {
+				handler.sendEmptyMessage(GO_GUIDE);
+				// handler.sendEmptyMessageDelayed(GO_GUIDE,
+				// SPLASH_DELAY_MILLIS);
+			} else {
+				handler.sendEmptyMessage(GO_HOME);
+				// handler.sendEmptyMessageDelayed(GO_HOME,
+				// SPLASH_DELAY_MILLIS);
+			}
+
 		}
+
 	}
 
 	/**
@@ -78,6 +130,7 @@ public class SplashActivity extends Activity {
 	public void goHome() {
 		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
+		overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
 		finish();
 	}
 
